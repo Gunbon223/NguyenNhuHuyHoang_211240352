1be63ff19667a9bf91953efa8ff480cf6b05b2fa
Gunbon223 <88621181+Gunbon223@users.noreply.github.com>
2023-11-08 08:02:57 +0700

    Add project files.

---
+++ NguyenNhuHuyHoang_211240352/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NguyenNhuHuyHoang_211240352.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
